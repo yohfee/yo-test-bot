18c79635ee238cfd0fdb5b5ad557695dd41ffcae
Yohei Takahashi <yohei.takahashi@rakuten.com>
2020-04-07 12:46:38 +0900

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const path = require("path");
 const Discord = require("discord.js");
 const OpenJTalk = require("openjtalk");
 const client = new Discord.Client();
-const mei = new OpenJTalk();
+const mazzo = new OpenJTalk({
+  htsvoice: path.join(__dirname, "mazzo.htsvoice")
+});
+
+let _connection;
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -9,40 +14,50 @@ client.on("ready", () => {
 
 client.on("message", message => {
   if (message.content === "ping") {
-    message.reply("pong");
-  } else {
-    if (!message.guild) return;
+    return message.reply("pong");
+  }
 
-    if (message.content === "/join") {
-      if (message.member.voiceChannel) {
-        message.member.voiceChannel
-          .join()
-          .then(connection => {
-            message.reply("I have successfully connected to the channel!");
-            mei._makeWav(message.content, 220, (error, result) => {
-              if (error) {
-                return console.log(error);
-              }
-              const dispatcher = connection.playFile(result.wav);
-              dispatcher.on("end", () => {
-                console.log("end: " + result.wav);
-              });
-              dispatcher.on("error", e => {
-                console.log("error: " + result.wav);
-                console.log(e);
-              });
-              dispatcher.setVolume(1);
-              console.log(dispatcher.time);
-              dispatcher.pause();
-              dispatcher.resume();
-              dispatcher.end();
-            });
-          })
-          .catch(console.log);
-      } else {
-        message.reply("You need to join a voice channel first!");
-      }
+  if (!message.guild) return;
+
+  if (message.content === "/join") {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("I have successfully connected to the channel!");
+          _connection = connection;
+        })
+        .catch(console.log);
+    } else {
+      message.reply("You need to join a voice channel first!");
     }
+
+    return;
+  }
+
+  if (_connection && message.content.startsWith("/mazzo ")) {
+    mazzo._makeWav(
+      message.content.replace(/^\/mazzo /, ""),
+      220,
+      (error, result) => {
+        if (error) {
+          return console.log(error);
+        }
+        const dispatcher = _connection.playFile(
+          path.join(__dirname, result.wav),
+          () => {
+            dispatcher.end();
+          }
+        );
+        dispatcher.on("end", () => {
+          console.log("end: " + result.wav);
+        });
+        dispatcher.on("error", e => {
+          console.log("error: " + result.wav);
+          console.log(e);
+        });
+      }
+    );
   }
 });
 
