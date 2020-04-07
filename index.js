df6d2d6d03a27312f59a0e7052b9787b35da5c6a
Yohei Takahashi <yohei.takahashi@rakuten.com>
2020-04-07 12:46:24 +0900

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
+const OpenJTalk = require("openjtalk");
 const client = new Discord.Client();
+const mei = new OpenJTalk();
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -8,6 +10,39 @@ client.on("ready", () => {
 client.on("message", message => {
   if (message.content === "ping") {
     message.reply("pong");
+  } else {
+    if (!message.guild) return;
+
+    if (message.content === "/join") {
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel
+          .join()
+          .then(connection => {
+            message.reply("I have successfully connected to the channel!");
+            mei._makeWav(message.content, 220, (error, result) => {
+              if (error) {
+                return console.log(error);
+              }
+              const dispatcher = connection.playFile(result.wav);
+              dispatcher.on("end", () => {
+                console.log("end: " + result.wav);
+              });
+              dispatcher.on("error", e => {
+                console.log("error: " + result.wav);
+                console.log(e);
+              });
+              dispatcher.setVolume(1);
+              console.log(dispatcher.time);
+              dispatcher.pause();
+              dispatcher.resume();
+              dispatcher.end();
+            });
+          })
+          .catch(console.log);
+      } else {
+        message.reply("You need to join a voice channel first!");
+      }
+    }
   }
 });
 
