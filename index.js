704f9d3808605fab40e3e56ddd13d9e4eadac158
yohfee <yohfee@gmail.com>
2021-01-22 15:10:33 +0900

    Update to current api version

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const path = require("path");
 const Discord = require("discord.js");
 const OpenJTalk = require("openjtalk");
+
+const {
+  BOT_ID,
+  BOT_TOKEN,
+  VOCAL_VOICE,
+  VOCAL_PITCH,
+} = process.env;
+
+const rules = [
+  ({ content }) => content === '/neko' && 'にゃーん',
+  ({ content }) => content
+];
+
 const client = new Discord.Client();
-const mazzo = new OpenJTalk({
-  htsvoice: path.join(__dirname, "mazzo.htsvoice")
+const vocal = new OpenJTalk({
+  htsvoice: path.join(__dirname, 'voices', VOCAL_VOICE)
 });
 
 let _connection;
@@ -12,7 +25,9 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("message", message => {
+client.on("message", async message => {
+  console.log(`Got a message: ${message.content}`);
+
   if (message.content === "ping") {
     return message.reply("pong");
   }
@@ -20,45 +35,44 @@ client.on("message", message => {
   if (!message.guild) return;
 
   if (message.content === "/join") {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel
-        .join()
-        .then(connection => {
-          message.reply("I have successfully connected to the channel!");
-          _connection = connection;
-        })
-        .catch(console.log);
+    if (message.member?.voice?.channel) {
+      try {
+        _connection = await message.member.voice.channel.join();
+        message.reply('I have successfully connected to the channel!');
+      } catch (e) {
+        console.error(e);
+        message.reply('I could not connect to the channel...');
+      }
     } else {
-      message.reply("You need to join a voice channel first!");
+      message.reply('You need to join a voice channel first!');
     }
 
     return;
   }
 
-  if (_connection && message.content.startsWith("/mazzo ")) {
-    mazzo._makeWav(
-      message.content.replace(/^\/mazzo /, ""),
-      220,
-      (error, result) => {
+  if (_connection) {
+    if (message.author.id === BOT_ID) {
+      return;
+    }
+
+    const content = rules.reduce((c, f) => c ? c : f(message), null);
+    if (content) {
+      vocal._makeWav(content, VOCAL_PITCH, (error, result) => {
         if (error) {
-          return console.log(error);
+          return console.error(error);
         }
-        const dispatcher = _connection.playFile(
-          path.join(__dirname, result.wav),
-          () => {
-            dispatcher.end();
-          }
-        );
-        dispatcher.on("end", () => {
-          console.log("end: " + result.wav);
-        });
-        dispatcher.on("error", e => {
-          console.log("error: " + result.wav);
-          console.log(e);
-        });
-      }
-    );
+
+        const file = path.join(__dirname, result.wav);
+        console.log(`Start playing: ${file}`);
+
+        const dispatcher = _connection.play(file);
+        dispatcher.on('debug', console.debug);
+        dispatcher.on('error', console.error);
+      });
+    }
+  } else {
+    console.log('There is no connection...');
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(BOT_TOKEN);
