dbc5f7d6732cd15a57686763d6c78284415b042a
yohfee <yohfee@gmail.com>
2021-02-14 19:10:38 +0900

    Modulize components

---
+++ src/bot.ts
@@ -0,0 +1,49 @@
+import { Client, Message } from 'discord.js';
+import { Say, create as createSay } from './say';
+
+type RuleResult = string | false;
+
+type Rule = ((message: Message) => RuleResult);
+
+const random = (max: number) => Math.floor(Math.random() * Math.floor(max))
+
+const randomVocal = (voices: string[], minPitch: number, pitchRange: number): Say =>
+  createSay(voices[random(voices.length)], minPitch + random(pitchRange));
+
+export const create = (token: string, voices: string[], minPitch: number, pitchRange: number, rules: Rule[]) => {
+  const members: { [id: string]: Say } = {};
+
+  const client = new Client().on('message', async message => {
+    console.debug(message);
+
+    const { guild, author: { id, bot, username }, content, member } = message;
+
+    if (!guild || bot) return;
+
+    console.log(`${username} inputs "${content}"`);
+
+    const channel = member?.voice.channel;
+    if (channel && channel.joinable) {
+      const connection = client.voice?.connections.get(channel.id) || await channel.join();
+
+      const text = rules.reduce((result, rule) => result ? result : rule(message), false as RuleResult);
+      if (text) {
+        console.log(`${username} says "${text}"`);
+
+        members[id] ||= randomVocal(voices, minPitch, pitchRange);
+
+        try {
+          const { file, dispose } = await members[id](text);
+          const dispatcher = connection.play(file);
+          dispatcher.on('finish', dispose);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  });
+
+  return {
+    run: () => client.login(token),
+  };
+}
---
+++ src/say.ts
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+
+const OpenJTalk = require('openjtalk');
+
+type MakeWavResult = {
+  wav: string
+};
+
+type MakeWavCallback = (error: Error, result: MakeWavResult) => void;
+
+type OpenJTalk = {
+  _makeWav: (text: string, pitch: number, callback: MakeWavCallback) => void
+};
+
+type SayResult = {
+  file: string
+  dispose: () => void
+};
+
+export type Say = (text: string) => Promise<SayResult>;
+
+export const create = (voice: string, pitch: number): Say => {
+  const vocal: OpenJTalk = new OpenJTalk({
+    htsvoice: voice,
+  });
+
+  return async text => new Promise((resolve, reject) =>
+    vocal._makeWav(text, pitch, (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      const file = path.join(__dirname, result.wav);
+      resolve({
+        file,
+        dispose: () => fs.unlinkSync(file),
+      });
+    })
+  );
+};
